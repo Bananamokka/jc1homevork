683a780b0c39634451b5f7495e8f02ce4c17de61
User <yanakonopskaya1998@gmail.com>
2022-05-13 21:44:30 +0300

    Initial Commit
    Signed-off-by: User <yanakonopskaya1998@gmail.com>

--- src/com/bananamokka/demo/TestClass.java
+++ src/com/bananamokka/demo/TestClass.java
@@ -3,14 +3,19 @@ package com.bananamokka.demo;
 public class TestClass {
     public static void main(String[] args) {
         try {
-          //  Cat[] CatsAr = new Cat[3];
-            //CatsAr[0].setCatName("Tom");
-            //CatsAr[1].setCatName("Jack");
-          /*  switch (CatsAr[0].getCatName()) {
-                case "Cat":
-                    System.out.println("We found it!");
-                    break;
-                default:
+        /*  Cat[] CatsAr = new Cat[3];
+            CatsAr[0].setCatName("Tom");
+            CatsAr[1].setCatName("Jack");
+            if (CatsAr[0].getCatName() == "Barsik") {
+                System.out.println("We found it!");
+            }
+            else if (CatsAr[1].getCatName() == "Barsik") {
+            System.out.println("We found it!");
+            }
+            else if (CatsAr[2].getCatName() == "Barsik") {
+                System.out.println("We found it!");
+            }
+            else CatsAr[0].someProcess();
             */
            Cat cat = new Cat();
            cat.someProcess();
